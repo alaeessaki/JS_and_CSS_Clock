43315af72bb37140aefe305c4f1532eecb04409d
Alae Es-saki <alaeessaki@gmail.com>
2019-02-28 17:04:59 +0100

    fixing some stuffs

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var hour_hands = document.getElementById("hrs")
 var min_hand = document.getElementById("mins")
 var second_hand = document.getElementById("scs")
 var isSet = false;
+
 function Horloge(){
   var time = new Date()
   seconds = time.getSeconds()
@@ -14,13 +15,16 @@ function Horloge(){
   Afficher(seconds,minutes,hours)
   console.log(hours + ":" + minutes + ":" + seconds)
 }
+
 function setTime(seconds, minutes, hours){
   sec_somme = 84 + seconds*6
   min_somme = 84 + minutes*6
   hour_somme = 90 + hours*30
   isSet = true;
 }
+
 setInterval(Horloge, 1000);
+
 function Afficher(seconds,minutes,hours){
   if(isSet == false){
     setTime(seconds, minutes, hours)
@@ -43,11 +47,8 @@ function Afficher(seconds,minutes,hours){
   else{
     hour_somme = 0;
   }
-  console.log(sec_somme)
-  console.log(min_somme)
-  console.log(hour_somme)
   hour_hands.style = "transform: rotate("+hour_somme+"deg);"
   min_hand.style = "transform: rotate("+min_somme+"deg);"
   second_hand.style = "transform: rotate("+sec_somme+"deg);"
 }
-console.log(isSet)
+
